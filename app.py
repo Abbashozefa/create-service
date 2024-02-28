8f85fac2465c224dc31075bbd17868dfac7fc299
Abbashozefa <abbashozefa@gmail.com>
2024-02-28 12:32:59 +0530

    DockerImages

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,jsonify
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def get_service_name():
+    return jsonify({'name':'HTTPLB'})
+
+
+if __name__=='__main__':
+    app.run()
+
