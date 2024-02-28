f5e3119edd094930b466b0d27b9c593f2463bfcc
Abbashozefa <abbashozefa@gmail.com>
2024-02-28 12:43:13 +0530

    Main branch

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+
+# os.system('helm create '+'charts')
+
+# os.system('cp pod.yaml charts/templates/')
+
+# os.system('rm -rf charts/')
+
+# os.system('helm upgrade charts charts/')
+
+# os.system('helm package charts/')
+
+# os.system('helm install deb1 charts-0.1.0.tgz')
---
+++ app3.py
@@ -0,0 +1,11 @@
+import os
+
+os.system('helm install '+'deb charts/')
+
+# os.system('cp pod.yaml charts2/templates/')
+
+# os.system('rm -rf charts/')
+
+# os.system('helm upgrade charts charts/')
+
+# os.system('helm package charts/')
